2b58e737579e501d4afc856f5fd5106c4540fc04
Maggie Battersby <maggiebat1436@gmail.com>
2024-11-14 14:35:39 -0500

    added the if condition for the pawn promotion file to be called if a pawn  gets to the other side. This is if the tile is empty or a piece is caputred in the final or first row. Currently cannot be tested because of bug with legal_moves and the not being able to move a piece to either of those rows.

--- front_end.py
+++ front_end.py
@@ -358,6 +358,8 @@ class ChessGUI:
             # Select the piece if any
             piece = self.board.piece_at(clicked_square)
             if piece and piece.color == self.is_white_turn:  # Ensure the player selects their own piece
+                if (self.board.piece_at(self.selected_square) == 'P' or self.board.piece_at(self.selected_square) == 'p') and (clicked_square >= 56 or clicked_square <= 7):
+                    promote(self.captured_pieces)
                 self.selected_square = clicked_square
                 # Display possible moves for the selected piece
                 self.show_possible_moves(clicked_square)
@@ -365,7 +367,7 @@ class ChessGUI:
             # Try to make a move
             move = chess.Move(self.selected_square, clicked_square)
             if move in self.board.legal_moves:
-                self.capture_piece(clicked_square)
+                self.capture_piece(clicked_square, self.selected_square)
                 self.board.push(move)
                 self.update_pieces()
 
@@ -401,7 +403,7 @@ class ChessGUI:
             # Reset selected square
             self.selected_square = None
 
-    def capture_piece(self, clicked_square):
+    def capture_piece(self, clicked_square, selected_square):
         captured_piece = self.board.piece_at(clicked_square)
         if captured_piece:
             # dictionary
@@ -409,7 +411,7 @@ class ChessGUI:
             self.captured_pieces[piece_color].append(captured_piece.symbol().upper())
             # table version
             self.cursor.execute("INSERT INTO captured (color, piece) VALUES (?, ?)", (piece_color, captured_piece.symbol()))
-        # if piece == P (ignore case) and row = 8: // this is just psudeocode needs to be replaced
+        if (self.board.piece_at(selected_square) == 'P' or self.board.piece_at(selected_square) == 'p') and (clicked_square >= 56 or clicked_square <= 7):
             promote(self.captured_pieces)
     
     def show_possible_moves(self, square):
